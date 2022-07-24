9d51fd517eaa6f014e9d34413804cafb73baf167
Valeri Karpov <val@karpov.io>
2022-07-24 12:08:57 -0400

    avoid catching errors in config files

--- netlify-functions-ecommerce/.config/index.js
+++ netlify-functions-ecommerce/.config/index.js
@@ -1,12 +1,8 @@
 'use strict';
 
 if (process.env.NODE_ENV) {
-  try {
-    module.exports = require('./' + process.env.NODE_ENV);
-    console.log('Using ' + process.env.NODE_ENV);
-  } catch (err) {
-    module.exports = require('./development');
-  }
+  module.exports = require('./' + process.env.NODE_ENV);
+  console.log('Using ' + process.env.NODE_ENV);
 } else {
   console.log('using production');
   module.exports = require('./production');
