bcc83ff509da1b2efe381b24801493d1070806b0
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-07-27 12:10:26 -0400

    fixed tests

--- netlify-functions-ecommerce/test/createOrder.test.js
+++ netlify-functions-ecommerce/test/createOrder.test.js
@@ -2,9 +2,8 @@
 
 const { describe, it } = require('mocha');
 const assert = require('assert');
-const { handler: addToCart } = require('../netlify/functions/addToCart');
-const { handler: checkout } = require('../netlify/functions/checkout');
 const { handler: createOrder } = require('../netlify/functions/createOrder');
+const { handler: addToCart } = require('../netlify/functions/addToCart');
 const fixtures = require('./fixtures');
 const sinon = require('sinon');
 const stripe = require('../integrations/stripe');
@@ -17,5 +16,54 @@ describe('Create Order', function() {
         { name: 'Another Test Product', price: 600 }
       ]
     }).then((res) => res.products);
+    sinon.stub(stripe.paymentIntents, 'retrieve').returns({ status: 'succeeded', id: '123', brand: 'visa', last4: '1234' });
+    sinon.stub(stripe.paymentMethods, 'retrieve').returns({ status: 'succeeded', id: '123', brand: 'visa', last4: '1234' });
+    const params = {
+      body: {
+        cartId: null,
+        items: [
+          { productId: products[0]._id, quantity: 2 },
+          { productId: products[1]._id, quantity: 1 }
+        ]
+      }
+    };
+    params.body = JSON.stringify(params.body);
+    const result = await addToCart(params);
+    result.body = JSON.parse(result.body);
+    console.log(result);
+    console.log(result.body.items)
+    sinon.stub(stripe.checkout.sessions, 'retrieve').returns({
+      status: 'succeeded', 
+      id: '123', 
+      brand: 'visa', 
+      last4: '1234' ,
+      metadata: {
+       products: JSON.stringify(result.body.items),
+       shippingType: 'standard'
+      },
+      customer_details: {
+       name: 'Test Testerson',
+       email: 'test@localhost.com',
+       address: {
+         line1: '12345 Syndey Street',
+         city: 'Miami',
+         state: 'Florida',
+         postal_code: '33145'
+       }
+      },
+      amount_total: 1600,
+   });
+    const newParams = {
+      body: {
+        cartId: result.body._id,
+        sessionId: '123',
+      }
+    };
+    newParams.body = JSON.stringify(newParams.body)
+    const order = await createOrder(newParams);
+    order.body = JSON.parse(order.body);
+    assert(order.body.order);
+    assert(order.body.cart);
+    assert(order.body.session);
   });
 });
