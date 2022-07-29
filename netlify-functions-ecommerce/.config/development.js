35b06586275f0c70183e502f62e39699601532b1
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-07-29 14:58:56 -0400

    removed key

--- netlify-functions-ecommerce/.config/development.js
+++ netlify-functions-ecommerce/.config/development.js
@@ -2,7 +2,7 @@
 
 module.exports = Object.freeze({
   mongodbUri: 'mongodb://localhost:27017/ecommerce',
-  stripeSecretKey: 'sk_test_51IkuAqIFSwo5WpGWnvqNvPP2sP7ESGuV8d9QQxYaM3oGdJa8JCyuFV1vTiLzZcIlpTKkk1SxRp39XifgjLhjQH46005KTEKpqF',
+  stripeSecretKey: '',
   stripeSuccessUrl: 'http://localhost:5000/Confirmation/confirmation',
   stripeCancelUrl: 'http://localhost:5000/Confirmation/confirmation'
 });
