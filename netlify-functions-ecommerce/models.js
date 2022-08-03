d87bdec79accfd9a771404642efe704f599a0bc6
Valeri Karpov <val@karpov.io>
2022-08-03 11:48:56 -0400

    rename -> stargate-mongoose

--- netlify-functions-ecommerce/models.js
+++ netlify-functions-ecommerce/models.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const { driver } = require('astra-mongoose');
+const { driver } = require('stargate-mongoose');
 mongoose.setDriver(driver);
 
 const productSchema = new mongoose.Schema({
