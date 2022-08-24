15ccbad8490b0455c103b529ed3f03091d87246e
Valeri Karpov <val@karpov.io>
2022-08-24 12:24:29 -0400

    drop collections after tests to make sure we are correctly creating collections on startup

--- netlify-functions-ecommerce/test/setup.test.js
+++ netlify-functions-ecommerce/test/setup.test.js
@@ -5,12 +5,17 @@ const connect = require('../connect');
 const mongoose = require('../mongoose');
 
 before(async function() {
-  this.timeout(30000);
+  this.timeout(30_000);
   await connect();
   await Promise.all(Object.values(mongoose.connection.models).map(Model => Model.init()));
   await Promise.all(Object.values(mongoose.connection.models).map(Model => Model.deleteMany({})));
 });
 
 after(async function() {
+  this.timeout(30_000);
+  await Promise.all(Object.values(mongoose.connection.models).map(async Model => {
+    await mongoose.connection.dropCollection(Model.collection.collectionName);
+  }));
+
   await mongoose.disconnect();
 });
