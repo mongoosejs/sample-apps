af8fa6c4ad06290a179865e1740a4c988b177ed9
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2022-11-15 14:23:40 -0500

    Netlify sample app tested and few minor fixes were made

--- netlify-functions-ecommerce/.config/index.js
+++ netlify-functions-ecommerce/.config/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
 if (process.env.NODE_ENV) {
-  module.exports = require('./' + process.env.NODE_ENV);
-  console.log('Using ' + process.env.NODE_ENV);
+  try {
+    module.exports = require('./' + process.env.NODE_ENV);
+    console.log('Using ' + process.env.NODE_ENV); 
+  } catch (error) {
+    module.exports = require('./development');
+  }
 } else {
   console.log('using production');
   module.exports = require('./production');
