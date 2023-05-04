37c27ae6492ea404dd0b1ec3f0f7b0d009a42196
Valeri Karpov <val@karpov.io>
2023-05-04 14:25:53 -0400

    disable create namespace on connect

--- netlify-functions-ecommerce/.config/production.js
+++ netlify-functions-ecommerce/.config/production.js
@@ -3,7 +3,8 @@
 module.exports = Object.freeze({
   jsonApiUrl: process.env.JSON_API_URL,
   jsonApiConnectOptions: {
-    isAstra: true
+    isAstra: true,
+    createNamespaceOnConnect: false
   },
   stripeSecretKey: 'test',
   stripeSuccessUrl: 'https://deploy-preview-1--amazing-cassata-75f094.netlify.app/order-confirmation',
